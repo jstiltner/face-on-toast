912853a340b7483563ba03e78e2bf30385ed672e
Jessawynne Parker <jessawynne@gmail.com>
2015-11-20 11:12:22 -0600

    working on getting all bower components

--- javascripts/3-loadtemplates.js
+++ javascripts/3-loadtemplates.js
@@ -1,12 +1,12 @@
 define(function(require){
   return {
-    findmovie: require("hbs!../templates/templates-findmovie"),
+    // findmovie: require("hbs!../templates/templates-findmovie"),
     login: require("hbs!../templates/templates-login"),
-    newuser: require("hbs!../templates/templates-newuser"),
-    searchmymovie: require("hbs!../templates/templates-searchmymovie"),
-    userhome: require("hbs!../templates/templates-userhome"),
-    userunwatched: require("hbs!../templates/templates-userunwatched"),
-    userwatched: require("hbs!../templates/templates-userwatched"),
+    // newuser: require("hbs!../templates/templates-newuser"),
+    // searchmymovie: require("hbs!../templates/templates-searchmymovie"),
+    // userhome: require("hbs!../templates/templates-userhome"),
+    // userunwatched: require("hbs!../templates/templates-userunwatched"),
+    // userwatched: require("hbs!../templates/templates-userwatched"),
   };
 
   function loginForm(loginObjects) {
