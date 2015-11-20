dc5c0c3654d083e1f780594ea299311beac0180d
Jessawynne Parker <jessawynne@gmail.com>
2015-11-20 13:15:58 -0600

    tyring to get the login viewe to load

--- javascripts/3-loadtemplates.js
+++ javascripts/3-loadtemplates.js
@@ -3,13 +3,13 @@ define(function(require){
   var $ = require("jquery");
 
   return {
-    // findmovie: require("hbs!../templates/template-findmovie"),
+    findmovie: require("hbs!../templates/template-findmovie"),
     login: require("hbs!../templates/template-login"),
-    // newuser: require("hbs!../templates/template-newuser"),
-    // searchmymovie: require("hbs!../templates/template-searchmymovie"),
-    // userhome: require("hbs!../templates/template-userhome"),
-    // userunwatched: require("hbs!../templates/template-userunwatched"),
-    // userwatched: require("hbs!../templates/template-userwatched"),
+    newuser: require("hbs!../templates/template-newuser"),
+    searchmymovie: require("hbs!../templates/template-searchmymovie"),
+    userhome: require("hbs!../templates/template-userhome"),
+    userunwatched: require("hbs!../templates/template-userunwatched"),
+    userwatched: require("hbs!../templates/template-userwatched"),
   };
 
   function loginForm(loginObjects) {
