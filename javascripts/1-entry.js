7cdc3c8f6601f73414c9c76f7a5796e8cbbe42fe
Jessawynne Parker <jessawynne@gmail.com>
2015-11-20 11:26:58 -0600

    trying to get all bower components up and running

--- javascripts/1-entry.js
+++ javascripts/1-entry.js
@@ -1,17 +1,17 @@
 require.config({
-  baseUrl: './javascripts',
+  baseUrl: "./javascripts",
   paths: {
-    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
-    'lodash': '../lib/bower_components/lodash/lodash.min',
-    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
-    'q': '../lib/bower_components/q/q',
-    'firebase': '../lib/bower_components/firebase/firebase',
-    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
+    "lodash": "../lib/bower_components/lodash/lodash.min",
+    "hbs": "../lib/bower_components/handlebars/handlebars.min",
+    "q": "../lib/bower_components/q/q",
+    "firebase": "../lib/bower_components/firebase/firebase",
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min"
   },
   shim: {
-    'bootstrap': ['jquery'],
-    'firebase': {
-      exports: 'Firebase'
+    "bootstrap": ["jquery"],
+    "firebase": {
+      exports: "Firebase"
     }
   }
 });
