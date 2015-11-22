4fc356a3eb14052cf062569ad554f3302e1a26d3
Jason Stiltner <jasonstiltner@gmail.com>
2015-11-22 17:11:37 -0600

    ajax call started

--- javascripts/eventhandlers.js
+++ javascripts/eventhandlers.js
@@ -9,10 +9,28 @@ define(function(require) {
   var populateUserHomeView = require("populateUserHomeView");
 
     $("#findMoviesBtn").click(function (e) {
-    	$("#view-find-movie").html(templates.findmovie());
-    	$("#findMovieModal").modal("toggle");
+  	$("#view-find-movie").html(templates.findmovie());
+  	$("#findMovieModal").modal("toggle");
 		$("#view-find-movie").removeClass("hidden");
 		$("#view-find-movie").addClass("visible");
+
+    $("findBtn").click(function getImage(title) { 
+    $.ajax({ 
+        type: "GET",
+        dataType: "json",
+        url: "http://www.omdbapi.com/?t=" + title,
+        success: function(data){
+          console.log("data returned", data);
+            return $.get(data.Poster); 
+        },
+        async:false,
+        error: function() {
+            return "Image not found.";
+        }
+    });
+}
+        })
+    }
 	});
 
 });
