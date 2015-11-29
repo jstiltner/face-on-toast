1d9e3927611b8f2548438404540546a2edf19f38
Jason Stiltner <jasonstiltner@gmail.com>
2015-11-28 23:12:07 -0600

    code is not pretty, but movie data is now pushed to firebase

--- javascripts/3-loadtemplates.js
+++ javascripts/3-loadtemplates.js
@@ -7,6 +7,7 @@ define(function(require){
 
     //filter options 
     searchresults: require("hbs!../templates/template-searchresults"), 
+    filteringresults: require("hbs!../templates/template-filteringresults"),
     //userallmovies: require("hbs!../templates/template-allmovies"),
     userunwatched: require("hbs!../templates/template-userunwatched"),
     userwatched: require("hbs!../templates/template-userwatched"),
--- javascripts/movies-to-FB.js
+++ javascripts/movies-to-FB.js
@@ -9,8 +9,56 @@ define(function(require) {
   $("body").on('click', '.add-movie', function(e){
 
     var movieRefID = this.id;
-    console.log("click", this.id);
+// Check to see if the movie already exists in FB database
+// get movieRef via IMDBid path in FB
+    var movieRef = new Firebase('https://faceontoast.firebaseio.com/movies/' + movieRefID);
+    console.log("movieRef variable holds", movieRef );
+// if path !== invalid
+  movieRef.on("value", function(snapshot) {
+  console.log("snapshot",snapshot.val() );
+  if (snapshot.val() === null){
 
+
+
+  // Ajax call to main OMDB for movie information
+  var deferred = Q.defer();
+    console.log("reached for info on movie:", movieRefID );
+            $.ajax({ 
+                type: "GET",
+                dataType: "json",
+                url: "http://www.omdbapi.com/?i=" + movieRefID})
+            //when done do the following:
+            .done(function(data) {    
+              // pass the results 
+              deferred.resolve(data);
+  // Record results
+            var title = data.Title;
+            var year = data.Year;
+            var actors = data.Actors;
+            var imdbID = data.imdbID;
+              var actorsArray = actors.split(", ");
+
+  //Flash results to FB
+        // build object to push to FB from data 
+        var objectforFB = {
+          'Title': title,
+          'Year': year,
+          'Actors': actorsArray
+  
+};
+  console.log("objectforFB", objectforFB );
+  var newMovieRef = new Firebase('https://faceontoast.firebaseio.com/movies/'+imdbID);      
+    newMovieRef.set(objectforFB);
+
+            // if the call errors
+            })
+            .fail(function(xhr, status, error) {
+              deferred.reject(error);
+            });
+
+  }
+
+});
     var nameRef = new Firebase('https://faceontoast.firebaseio.com/users/' + userStorage.getUid());
     console.log("userStorage.getUid()", userStorage.getUid());
 
@@ -19,7 +67,9 @@ define(function(require) {
   
     nameRef.child("movieRefs").push(movieRefID);
   
-        $(this).parent().remove();
+    $(this).parent().remove();
+
+
   });
 
   return {
